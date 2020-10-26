a989318c62ede5b123b517852dbab943d29a9c8f
lucasnpc <31970919+lucasnpc@users.noreply.github.com>
2020-10-26 02:41:09 -0300

    Implementação de rotina para exportar PDF

--- Farmácia de Manipulação/Views/FrmRelatorios.Designer.cs
+++ Farmácia de Manipulação/Views/FrmRelatorios.Designer.cs
@@ -28,19 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridRelatorios = new System.Windows.Forms.DataGridView();
+            this.bRelatorioVenda = new System.Windows.Forms.Button();
+            this.exportarPdf = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridRelatorios)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridRelatorios
+            // 
+            this.dataGridRelatorios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridRelatorios.Location = new System.Drawing.Point(12, 76);
+            this.dataGridRelatorios.Name = "dataGridRelatorios";
+            this.dataGridRelatorios.RowHeadersWidth = 51;
+            this.dataGridRelatorios.RowTemplate.Height = 24;
+            this.dataGridRelatorios.Size = new System.Drawing.Size(1084, 547);
+            this.dataGridRelatorios.TabIndex = 0;
+            // 
+            // bRelatorioVenda
+            // 
+            this.bRelatorioVenda.BackColor = System.Drawing.Color.DodgerBlue;
+            this.bRelatorioVenda.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bRelatorioVenda.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.bRelatorioVenda.Location = new System.Drawing.Point(31, 12);
+            this.bRelatorioVenda.Name = "bRelatorioVenda";
+            this.bRelatorioVenda.Size = new System.Drawing.Size(197, 58);
+            this.bRelatorioVenda.TabIndex = 1;
+            this.bRelatorioVenda.Text = "GERAR RELATÓRIO DE VENDAS";
+            this.bRelatorioVenda.UseVisualStyleBackColor = false;
+            this.bRelatorioVenda.Click += new System.EventHandler(this.bRelatorioVenda_Click);
+            // 
+            // exportarPdf
+            // 
+            this.exportarPdf.BackColor = System.Drawing.Color.DodgerBlue;
+            this.exportarPdf.Enabled = false;
+            this.exportarPdf.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exportarPdf.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.exportarPdf.Location = new System.Drawing.Point(923, 12);
+            this.exportarPdf.Name = "exportarPdf";
+            this.exportarPdf.Size = new System.Drawing.Size(154, 58);
+            this.exportarPdf.TabIndex = 2;
+            this.exportarPdf.Text = "EXPORTAR PDF";
+            this.exportarPdf.UseVisualStyleBackColor = false;
+            this.exportarPdf.Click += new System.EventHandler(this.exportarPdf_Click);
             // 
             // FrmRelatorios
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(1108, 635);
+            this.Controls.Add(this.exportarPdf);
+            this.Controls.Add(this.bRelatorioVenda);
+            this.Controls.Add(this.dataGridRelatorios);
             this.Name = "FrmRelatorios";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmRelatorios";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridRelatorios)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridRelatorios;
+        private System.Windows.Forms.Button bRelatorioVenda;
+        private System.Windows.Forms.Button exportarPdf;
     }
 }
--- Farmácia de Manipulação/Views/FrmRelatorios.cs
+++ Farmácia de Manipulação/Views/FrmRelatorios.cs
@@ -1,20 +1,145 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using Farmácia_de_Manipulação.Controladores;
+using Farmácia_de_Manipulação.Models;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Npgsql;
 
 namespace Farmácia_de_Manipulação.Views
 {
     public partial class FrmRelatorios : Form
     {
+        List<Venda> vendas = new List<Venda>();
+
         public FrmRelatorios()
         {
             InitializeComponent();
+
+            /*Document document = new Document(PageSize.A4);
+            document.SetMargins(40, 40, 40, 80);
+            document.AddCreationDate();
+
+            string caminho = @"C:\Users\Lucas\Documents\teste.pdf";
+
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(caminho, FileMode.Create));
+
+            document.Open();
+            string dados = "Código Venda Código Produto Código Cliente Código Funcionário Quantidade Valor Venda\n";
+            if (controle.Equals("1"))
+            {
+                Paragraph paragraph = new Paragraph(dados, new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.TIMES_ROMAN, 12));
+                paragraph.Alignment = Element.ALIGN_JUSTIFIED;
+                foreach (Venda v in vendas)
+                {
+                    paragraph.Add(v.cod_venda + "     " + v.cod_produto + "      " + v.cod_cliente + "      " + v.cod_funcionario + "      " +
+                        v.quantidade + "      " + v.valor_venda+"\n");
+                }
+                document.Add(paragraph);
+                document.Close();
+            }*/
+        }
+
+        private void bRelatorioVenda_Click(object sender, EventArgs e)
+        {
+            vendas = new VendaDAO().GetVendas();
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Código Venda", typeof(string));
+            dt.Columns.Add("Código Produto", typeof(string));
+            dt.Columns.Add("Código Cliente", typeof(string));
+            dt.Columns.Add("Código Funcionário", typeof(string));
+            dt.Columns.Add("Quantidade", typeof(string));
+            dt.Columns.Add("Valor Venda", typeof(string));
+
+            foreach (Venda v in vendas)
+            {
+                DataRow dr = dt.NewRow();
+                dr["Código Venda"] = v.cod_venda;
+                dr["Código Produto"] = v.cod_produto;
+                dr["Código Cliente"] = v.cod_cliente;
+                dr["Código Funcionário"] = v.cod_funcionario;
+                dr["Quantidade"] = v.quantidade.ToString();
+                dr["Valor Venda"] = v.valor_venda.ToString();
+
+                dt.Rows.Add(dr);
+            }
+            dataGridRelatorios.DataSource = dt;
+            dataGridRelatorios.Update();
+            exportarPdf.Enabled = true;
+        }
+
+        private void exportarPdf_Click(object sender, EventArgs e)
+        {
+            if (dataGridRelatorios.Rows.Count > 0)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "PDF (*.pdf)|*.pdf";
+                sfd.FileName = @"C:\Users\Lucas\Documents\teste.pdf";
+                bool fileError = false;
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists(sfd.FileName))
+                    {
+                        try
+                        {
+                            File.Delete(sfd.FileName);
+                        }
+                        catch (IOException ex)
+                        {
+                            fileError = true;
+                            MessageBox.Show("It wasn't possible to write the data to the disk." + ex.Message);
+                        }
+                    }
+                    if (!fileError)
+                    {
+                        try
+                        {
+                            PdfPTable pdfTable = new PdfPTable(dataGridRelatorios.Columns.Count);
+                            pdfTable.DefaultCell.Padding = 3;
+                            pdfTable.WidthPercentage = 100;
+                            pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+
+                            foreach (DataGridViewColumn column in dataGridRelatorios.Columns)
+                            {
+                                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                                pdfTable.AddCell(cell);
+                            }
+
+                            foreach (Venda v in vendas)
+                            {
+                                pdfTable.AddCell(v.cod_venda);
+                                pdfTable.AddCell(v.cod_produto);
+                                pdfTable.AddCell(v.cod_cliente);
+                                pdfTable.AddCell(v.cod_funcionario);
+                                pdfTable.AddCell(v.quantidade.ToString());
+                                pdfTable.AddCell(v.valor_venda.ToString());
+                            }
+
+                            using (FileStream stream = new FileStream(sfd.FileName, FileMode.Create))
+                            {
+                                Document pdfDoc = new Document(PageSize.A4, 10f, 20f, 20f, 10f);
+                                PdfWriter.GetInstance(pdfDoc, stream);
+                                pdfDoc.Open();
+                                pdfDoc.Add(pdfTable);
+                                pdfDoc.Close();
+                                stream.Close();
+                            }
+
+                            MessageBox.Show("Dados Exportados com sucesso !!!");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Erro :" + ex.Message);
+                        }
+                    }
+                }
+            }
+            else
+                MessageBox.Show("Sem registros para exportar !!!");
         }
     }
 }
